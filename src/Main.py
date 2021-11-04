26cee8c31559c1c34af009aa4eb5adac8e6fe94d
flan7543 <93159148+flan7543@users.noreply.github.com>
2021-11-04 17:34:09 -0600

    import-data i don't know if i did this right, it's not able to read the data, but not sure what else to do so just pushing this through for now and i'll ask the prof tomorrow

--- src/Main.py
+++ src/Main.py
@@ -1,2 +1,7 @@
-data = "howdy"
-print(data)
+import csv as csv
+
+with open('Fake.csv', newline='') as csvfile:
+    fake = list(csv.reader(csvfile)) 
+
+with open('True.csv', newline='') as csvfile:
+    true = list(csv.reader(csvfile)) 
