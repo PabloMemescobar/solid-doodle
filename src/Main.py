657303d29cedc9fff616d03297afd893190ee8ab
Ramiro Piquer <44074611+PabloMemescobar@users.noreply.github.com>
2021-11-04 19:10:50 -0600

    Organize and path directory

--- src/Main.py
+++ src/Main.py
@@ -1,2 +1,15 @@
-data = "hi"
-print(data)
+import os
+import csv
+
+# Path to the data
+DATA_DIRECTORY = os.path.dirname(os.getcwd()) + "\\Data"
+
+# Fake data and real data files 
+fakeData = DATA_DIRECTORY + "\\Fake.csv"
+trueData = DATA_DIRECTORY + "\\True.csv"
+
+# Opening fakeData
+with open(fakeData, newline ='') as csvfile:
+  fake = csv.reader(csvfile, delimiter =' ')
+  for row in fake:
+    print(row)
