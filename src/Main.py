deb0092fe4942d0d2a8e3798d25e5417c424af3e
Ramiro Piquer <44074611+PabloMemescobar@users.noreply.github.com>
2021-11-04 20:05:03 -0600

    mac and windows compatible

--- src/Main.py
+++ src/Main.py
@@ -1,16 +1,17 @@
 # Modules
 import os
 import csv
+from pathlib import Path
 
 # Constants
-DATA_DIRECTORY = os.path.dirname(os.getcwd()) + "\\Data"
+DATA_DIRECTORY = Path(os.path.dirname(os.getcwd()) + "/Data/")
 
 # Fake data and real data files 
-fakeData = DATA_DIRECTORY + "\\Fake.csv"
-trueData = DATA_DIRECTORY + "\\True.csv"
+fakeData = DATA_DIRECTORY / "Fake.csv"
+trueData = DATA_DIRECTORY / "True.csv"
 
 # Opening fakeData
 with open(fakeData, newline ='') as csvfile:
-  fake = csv.reader(csvfile, delimiter =' ')
+  fake = csv.reader(csvfile, delimiter = ' ')
   for row in fake:
     print(row)
