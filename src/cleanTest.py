fdcb7333a3b7361f15ec5534f91d0c59c009bc84
flan7543 <93159148+flan7543@users.noreply.github.com>
2021-11-29 20:26:45 -0700

    update to all code
    Lots changed, but the overview is that I took all the functions we made (data cleaning/processing, making dictionaries, classifier) and put them in the code, create 5 sets of training/testing data and ran the code on these 5 sets of data, which outputs the accuracy. Still to do: create function for user input, visuals & ensure everything is annotated.

--- src/cleanTest.py
+++ src/cleanTest.py
@@ -1,23 +1,59 @@
-# In[1]:
+# In[1]: Import packages
 
 #Modules
 import os
 import csv
 from pathlib import Path
 import math
-import re
 from sklearn.model_selection import train_test_split
 from badWords import badWordsTable
-import re; pattern = re.compile('[^a-zA-Z_]+')
+import re; pattern = re.compile('[^a-zA-Z]+')
 import time
 import multiprocessing
 from multiprocessing import Pool
 from timeit import default_timer as timer
 import sys
+import random
+import numpy as np
+import pandas as pd
 
 # Constants
+# Ensuring that everyone running the code can access the data
 DATA_DIRECTORY = Path(os.path.dirname(os.getcwd()) + "/Data/")
 
+# In[2]: Preparing data
+with open(DATA_DIRECTORY / "Fake.csv", encoding = "utf8") as csvfile:
+    fakeData = list(csv.reader(csvfile))
+
+with open(DATA_DIRECTORY / "True.csv", encoding = "utf8") as csvfile:
+  trueData = list(csv.reader(csvfile))
+
+def getText(data):
+  text = []
+  for i in range(1, len(data)):
+      text.append([data[i][1]]) # The article text is located at [1] (second column in the data)
+  return text
+
+def getTextAddLabel(data, str):
+  text = []
+  for i in range(1, len(data)):
+      text.append([data[i][1], str]) # The article text is located at [1] (second column in the data)
+  return text
+
+fakeData = getTextAddLabel(fakeData, "fake")
+trueData = getTextAddLabel(trueData, "true")
+
+data = fakeData + trueData
+
+def getLabel(data):
+  label_list = []
+  for article in data:
+    label_list.append(article[1])
+  return label_list
+
+label_list = getLabel(data)
+
+# In[3]: Data Cleaning and Processing 
 def removeColumnAndLowercase(data):
   counter = 0
   for articles in data:
@@ -57,112 +93,165 @@ def cleanData(data):
 
   return data
 
-if __name__=="__main__":
-  with open(DATA_DIRECTORY / "Fake.csv", encoding = "utf8") as csvfile:
-    fakeData = list(csv.reader(csvfile))
+fakeData = cleanData(fakeData)
+trueData = cleanData(trueData)
 
-  with open(DATA_DIRECTORY / "True.csv", encoding = "utf8") as csvfile:
-    trueData = list(csv.reader(csvfile))
+data = fakeData + trueData
 
-  print("Computer core count: " + str(multiprocessing.cpu_count()))
+# In[4]: Make dictionaries
 
-  computerCoreCount = multiprocessing.cpu_count()
-  data = fakeData + trueData
-  masterDataTable = []
-  start = timer()
-  chunks = [data[x:x+computerCoreCount] for x in range(0, len(data), computerCoreCount)]
-
-  with Pool(processes = computerCoreCount) as pool:
-    threadData = [pool.apply_async(cleanData, (chunks[i],)) for i in range(len(chunks))]
-    masterDataTable+=[sections.get(timeout=100) for sections in threadData]
-
-    result = pool.apply_async(time.sleep, (10,))
-
-  print("Runtime: ", timer()-start)
-  #show articles
-  # for articles in masterDataTable:
-  #   print(articles)
-  #   print("\n \n \n")
-
-  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]
-
-  # [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], [20, 21, 22, 23, 24, 25, 26, 27, 28, 29], [30, 31, 32, 33, 34]]
-
-
-# @everyone It would be good if today in class everyone could run the `cleanTest.py` file and take a screenshot of the output 
-  #split the data into train and test samples - sklearn
-  #20% of the dataset will be use to test; 'random_state' could be any number
-
-  # Xdata, Ydata = np.arange(10).reshape((5, 2)), range(5)
-
-  X_train, X_test, y_train, y_test = train_test_split(masterDataTable, masterDataTable, test_size=0.2, random_state=88)
-
-  # #creating a two dictionaries; one that contains all words in real articles and one that contains all words in fake articles
-  true_dict = {}
-  fake_dict = {}
-
-  #turning the dataframes into lists to index
-  label_list = y_train
-  articles = X_train
-
-  #to be able to loop through each word in the article, we need to take the list of strings and turn it into a list that holds a list of words for each article
+def makeDictionary(data, labels): #Data is a list of lists containing a single string with the cleaned text
   article_list = []
+  for article in data:
+    article_list.append(article[0].split()) #Turn each single string article into a list of strings for each word
 
-  for article in masterDataTable:
-    try:
-      article_list.append(article.split())
-    except AttributeError:
-      print("incorrect attribute, flan fix here line 109, code shutting down...")
-      sys.exit()
-
-  # try:
-  #   for article in masterDataTable:
-  #     article_list.append(article.split())
-  #     break
-  # except TypeError:
-  #   print("incorrect type, code shutting down...")
-  #   sys.exit()
-  # else:
-  #   print("incorrect type, code shutting down...")
-  #   sys.exit()
-
-  
-  #loops through each article, check whether its true or false to put the words in correct dictionary, and then loops through the list of words
+  fake_dict = {}
+  true_dict = {}
   i = 0
-  while i < len(articles):
-      if label_list[i] == "true":
-          for word in article_list[i]:
-              if word not in true_dict: #create an entry in the dict if it doesn't exist already
-                  true_dict[word] = 0
-              true_dict[word] += 1
+  while i < len(article_list):
+    if labels[i] == "true":
+        for word in article_list[i]:
+            if word not in true_dict: #create an entry in the dict if it doesn't exist already
+                true_dict[word] = 0
+            true_dict[word] += 1
+    else:
+        for word in article_list[i]:
+            if word not in fake_dict:
+                fake_dict[word] = 0
+            fake_dict[word] += 1
+    i += 1
+  return fake_dict, true_dict
+
+fake_dict, true_dict = makeDictionary(data, label_list)
+
+# In[5]: Create all values needed for the Naive Bayes Classifier
+
+#function that counts adds together all values in dictionary
+def dict_count_words(dict):
+    count = 0
+    for key in dict:
+        count += dict[key]
+    return count
+
+#create dictionaries of probabilities of each word for both fake and true dictionaries
+def dict_create_probabilities(dict, count):
+    prob_dict = {}
+    for key in dict:
+        prob_dict[key] = dict[key]/count
+    return prob_dict
+
+#create count of words for both fake and true
+fake_word_count = dict_count_words(fake_dict)
+true_word_count = dict_count_words(true_dict)
+
+#create probability dictionaries for both fake and true
+fake_prob_dict = dict_create_probabilities(fake_dict, fake_word_count)
+true_prob_dict = dict_create_probabilities(true_dict, true_word_count)
+
+#create probabilities of article being fake or true using testing data
+fake_prob = label_list.count("fake")/len(label_list)
+true_prob = 1-fake_prob
+
+# In[6]: Create Classifier
+# articles is a list of lists where each list contains a cleaned article as 1 single string
+
+# P(A|B) = P(B|A)*P(A)
+# P(B|A) = P(word1 | true) * P(word2 | True) .... 
+
+def true_probability_list(articles):
+  true_prob_list = [] #P(true|B) for each article
+  for article in articles:
+    words = article[0].split()
+    conditional_true = 0 #P(B|A)
+    for word in words:
+      if word in true_prob_dict:
+        conditional_true += math.log(true_prob_dict[word]+(1/true_word_count)) # for each word in the article, we calculate P(word|true) and add the probabilities together (since they are logs)
       else:
-          for word in article_list[i]:
-              if word not in fake_dict:
-                  fake_dict[word] = 0
-              fake_dict[word] += 1
-      i += 1
+        conditional_true += math.log(1/true_prob)
+    true_prob_list.append(conditional_true+math.log(true_prob)) #log(P(B|A)) + log(P(A))
+  return true_prob_list
 
-  #create all values needed for the Naive Bayes Classifier
+def fake_probability_list(articles):
+  fake_prob_list = [] #P(fake|B) for each article
+  for article in articles:
+    words = article[0].split()
+    conditional_fake = 0 #P(B|A)
+    for word in words:
+      if word in fake_prob_dict:
+        conditional_fake += math.log(fake_prob_dict[word]+(1/fake_word_count)) # for each word in the article, we calculate P(word|fake) and add the probabilities together (since they are logs)
+      else:
+        conditional_fake += math.log(1/fake_prob)
+    fake_prob_list.append(conditional_fake+math.log(fake_prob)) #P(B|A) * P(A)
+  return fake_prob_list
 
-  #function that counts adds together all values in dictionary
-  def dict_count_words(dict):
-      count = 0
-      for key in dict:
-          count += dict[key]
-      return count
+# this function will allow us to test the accuracy of our program by also including the list of labels for each article (i.e. whether they're true or not)
+def test_true_or_fake(true_probs, fake_probs, label_list): # takes in the lists created from the true_probability_list and fake_probability_list functions
+    i = 1
+    results = []
+    while i < len(true_probs)+1: # compares each index in both lists and determines which value is bigger (i.e. if P(true|article) > P(fake|article) then return "Article i is True")
+        if true_probs[i-1] > fake_probs[i-1]:
+            results.append("true")
+        else:
+            results.append("fake")
+        i += 1
+    count = 0
+    i = 0
+    while i < len(results): # Here we are comparing the list inputed with the list created in the program to determine the accuracy
+        if results[i] == label_list[i]:
+            count += 1
+        i += 1
+    print("The accuracy is", count/len(results))
 
-  #create dictionaries of probabilities of each word for both fake and true dictionaries
-  def dict_create_probabilities(dict, count):
-      prob_dict = {}
-      for key in dict:
-          prob_dict[key] = dict[key]/count
-      return prob_dict
+def true_or_fake(true_probs, fake_probs):
+    while i in range(len(true_probs)+1):
+        results = []
+        if true_probs[i-1] > fake_probs[i-1]:
+            print("Article " + str(i) + " is True.")
+            results.append("true")
+        else:
+            print("Article " + str(i) + " is Fake.")
+            results.append("true")
+
+# In[7]: Creating testing and training data
+
+# Split articles into five groups
+n = 5
+random.shuffle(data)
+five_split = np.array_split(data, n)
+
+# Create article list and label list for each group
+def makeLists(list): # Make 2 lists from list of list containing 2 items
+  article_list = []
+  label_list = []
+  for article in list:
+    article_list.append([article[0]])
+    label_list.append(article[1])
+  return article_list, label_list
+    
+test1, test_label1 = makeLists(five_split[0])
+test2, test_label2 = makeLists(five_split[1])
+test3, test_label3 = makeLists(five_split[2])
+test4, test_label4 = makeLists(five_split[3])
+test5, test_label5 = makeLists(five_split[4])
+
+train1, train_label1 = (test2 + test3 + test4 + test5), (test_label2 + test_label3 + test_label4 + test_label5)
+train2, train_label2 = (test1 + test3 + test4 + test5), (test_label1 + test_label3 + test_label4 + test_label5)
+train3, train_label3 = (test1 + test2 + test4 + test5), (test_label1 + test_label2 + test_label4 + test_label5)
+train4, train_label4 = (test1 + test2 + test3 + test5), (test_label1 + test_label2 + test_label3 + test_label5)
+train5, train_label5 = (test1 + test2 + test3 + test4), (test_label1 + test_label2 + test_label3 + test_label4)
+
+# Create five sets of train/test data
+test_train = [[test1, test_label1, train1, train_label1], [test2, test_label2, train2, train_label2], [test3, test_label3, train3, train_label3], [test4, test_label4, train4, train_label4], [test5, test_label5, train5, train_label5]]
+
+# In[8]: Cross Validation
+
+for set in test_train:
+  #create dictionaries
+  fake_dict, true_dict = makeDictionary(set[2], set[3])
 
   #create count of words for both fake and true
   fake_word_count = dict_count_words(fake_dict)
   true_word_count = dict_count_words(true_dict)
-
-  avg_words = (fake_word_count + true_word_count) / 2
 
   #create probability dictionaries for both fake and true
   fake_prob_dict = dict_create_probabilities(fake_dict, fake_word_count)
@@ -172,68 +261,42 @@ if __name__=="__main__":
   fake_prob = label_list.count("fake")/len(label_list)
   true_prob = 1-fake_prob
 
-  def true_probability_list(articles):
-      true_prob_list = []
-      for article in articles:
-          conditional_true = 0 #P(B|A)
-          for word in article:
-              if word in true_prob_dict:
-                  conditional_true += math.log(true_prob_dict[word]) # for each word in the article, we calculate P(word|true) and add the probabilities together (since they are logs)
-              #else:
-                  #conditional_true += math.log(10000/true_prob)
-          true_prob_list.append(conditional_true*true_prob) #P(B|A) * P(A)
-      return true_prob_list
+  #find probailities from classifier
+  true_probs = true_probability_list(set[0])
+  fake_probs = fake_probability_list(set[0])
 
-  def fake_probability_list(articles):
-      fake_prob_list = []
-      for article in articles:
-          conditional_fake = 0 #P(B|A)
-          for word in article:
-              if word in fake_prob_dict:
-                  conditional_fake += math.log(fake_prob_dict[word]) # for each word in the article, we calculate P(word|fake) and add the probabilities together (since they are logs)
-              #else:
-                  #conditional_fake += math.log(10000/fake_prob)
-          fake_prob_list.append(conditional_fake*fake_prob) #P(B|A) * P(A)
-      return fake_prob_list
+  #determine accuracy of algorithm
+  test_true_or_fake(true_probs, fake_probs, set[1])
 
-  # this function will allow us to test the accuracy of our program by also including the list of labels for each article (i.e. whether they're true or not)
-  def test_true_or_fake(true_probs, fake_probs, label_list): # takes in the lists created from the true_probability_list and fake_probability_list functions
-      i = 1
-      results = []
-      while i < len(true_probs)+1: # compares each index in both lists and determines which value is bigger (i.e. if P(true|article) > P(fake|article) then return "Article i is True")
-          if true_probs[i-1] > fake_probs[i-1]:
-              # print("Article " + str(i) + " is True.")
-              results.append("true")
-          else:
-              # print("Article " + str(i) + " is Fake.")
-              results.append("fake")
-          i += 1
-      
-      count = 0
-      i = 0  
-      while i < len(results): # Here we are comparing the list inputed with the list created in the program to determine the accuracy
-          if results[i] == label_list[i]:
-              count += 1
-          i += 1
-      print("The accuracy is", count/len(results))
+# In[9]: User input
 
-  def true_or_fake(true_probs, fake_probs):
-      while i in range(len(true_probs)+1):
-          results = []
-          if true_probs[i-1] > fake_probs[i-1]:
-              # print("Article " + str(i) + " is True.")
-              results.append("true")
-          else:
-              # print("Article " + str(i) + " is Fake.")
-              results.append("true")
+user_input = input("Please enter file path: ")
 
-  test_articles = X_test
-  labels = y_test
-  # print(type(test_articles))
-  # print(type(true_probability_list(test_articles)))
-  # cleaning_data(test_articles)
-  test_true_or_fake(true_probability_list(test_articles), fake_probability_list(test_articles), labels)
+# THIS NEEDS TO BE WORKED ON STILL - WILL BE UPDATED NOV 30
+# def classifier(user_input, data):
+  # articles = getText(data)
+  # articles = cleanData(articles)
+  
+  # full_data = getText(data)
 
-  # 0.0.5739529706414359 using logs, but before adding in else statement
-  # 0.49019062195631 using logs and adding in else statement with 1/prob
+  # fake_dict, true_dict = makeDictionary(full_data, label_list)
+
+  #create count of words for both fake and true
+  # fake_word_count = dict_count_words(fake_dict)
+  # true_word_count = dict_count_words(true_dict)
+
+  #create probability dictionaries for both fake and true
+  # fake_prob_dict = dict_create_probabilities(fake_dict, fake_word_count)
+  # true_prob_dict = dict_create_probabilities(true_dict, true_word_count)
+
+  #create probabilities of article being fake or true using testing data
+  # fake_prob = label_list.count("fake")/len(label_list)
+  # true_prob = 1-fake_prob
+
+  #find probailities from classifier
+  # true_probs = true_probability_list(articles)
+  # fake_probs = fake_probability_list(articles)
+
+  #determine accuracy of algorithm
+  # true_or_fake(true_probs, fake_probs)
 
